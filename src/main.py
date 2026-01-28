28d329efcb9c81563f004ef6c73970d29e4bcd90
thefool309 <themagician309@gmail.com>
2026-01-27 20:59:27 -0600

    initial commit from local

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+def main():
+    print("Getting a deeper understanding of machine learning and data visualization")
+    
+
+if __name__ == "__main__":
+    main()
