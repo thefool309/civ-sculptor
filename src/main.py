f766f4f0013f52a4b15b54d94288c38b3cbd7c6a
thefool309 <themagician309@gmail.com>
2026-01-28 14:39:03 -0600

    updated main.py to reflect build changes

--- src/main.py
+++ src/main.py
@@ -12,13 +12,13 @@ build_dir = os.path.abspath(build_dir)
 
 sys.path.append(build_dir)
 
-import bindings
+import civ_module
 
 def main():
     a = 2
     b = 2
     print("Getting a deeper understanding of machine learning and data visualization")
-    print("the result of our c++ function is", bindings.add(a, b))
+    print("the result of our c++ function is", civ_module.add(a, b))
     print("managed to build c++ function that runs in python")
 
 if __name__ == "__main__":
