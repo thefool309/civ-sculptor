a35912d88235e6edc0b280d4e07f340a74a18fa8
thefool309 <themagician309@gmail.com>
2026-01-29 00:18:00 -0600

    fixed issues with linux build

--- src/include/RandomEngine.h
+++ src/include/RandomEngine.h
@@ -1,32 +1,30 @@
 #pragma once
-#include <random>
 #include <optional>
+#include <random>
 
-enum Biome {
-	COUNT_BI
-};
+enum Biome { COUNT_BI };
 
 struct CivSeed {
-	uint64_t rng_seed;
-	std::string name;
+  uint64_t rng_seed;
+  std::string name;
 
-	std::optional<int> tech_level;
-	std::optional<Biome> biome;
+  std::optional<int> tech_level;
+  std::optional<Biome> biome;
 
-	explicit CivSeed(uint64_t _seed, std::string _name) : rng_seed(_seed), name(_name) {}
+  explicit CivSeed(uint64_t _seed, std::string _name)
+      : rng_seed(_seed), name(_name) {}
 };
 
 class RandomEngine {
 private:
-	std::mt19937 rng;
+  std::mt19937 rng;
 
 public:
-	explicit RandomEngine(uint64_t seed) : rng(seed) {}
+  explicit RandomEngine(uint64_t seed) : rng(seed) {}
 
-	float generate_uniform_float(float min = 0.0, float max = 1.0);
+  float generate_uniform_float(float min = 0.0, float max = 1.0);
 
-	float geneerate_uniform_int(int min, int max);
+  float generate_uniform_int(int min, int max);
 
-	template<typename Enum>
-	Enum generateRandomEnum(int count);
+  template <typename Enum> Enum generateRandomEnum(int count);
 };
