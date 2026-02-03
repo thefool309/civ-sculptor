325387de446873d822ecd4bb3fb6f84ab3b5e2f1
thefool309 <themagician309@gmail.com>
2026-02-03 10:39:32 -0600

    made optimizations to map.h

--- src/services/Map.cpp
+++ src/services/Map.cpp
@@ -1,5 +1,6 @@
 #include "Map.h"
 #include "Tile.h"
+#include "RandomEngine.h"
 #include <iostream>
 /// <summary>
 /// Retrieve a Tile object with x and y coordinates
@@ -42,6 +43,8 @@ std::vector<Tile*>& Map::getNeighbors(Tile& tile) {
 }
 
 /// <summary>
+/// seeded growth algorithm for map generation
+/// 
 /// Populate the map's tiles with biome types using a randomized seeding and probabilistic spreading algorithm.
 /// For each terrain type (except Water) a random seed tile is chosen, then biomes spread to neighboring 
 /// Water tiles using a (Breadth-first Search) BFS-like queue with a 60% chance to convert each neighbor.
@@ -49,70 +52,114 @@ std::vector<Tile*>& Map::getNeighbors(Tile& tile) {
 /// <param name="rng">- Random engine used to generate uniform integers and floats.
 /// It is used to pick seed coordinates within Const::MAP_HEIGHT and Const::MAP_WIDTH and 
 /// to roll the 60% spread chance for neighbors. Passed by value.</param>
-void Map::generateMapTiles(RandomEngine rng) {
-    constexpr int numOfBiomes = static_cast<int>(TerrainType::COUNT_TT);
-    std::pair<int, int> seedCoords[numOfBiomes];
+void Map::generateMapTiles(RandomEngine& rng) {
 
-    std::vector<Tile*> seeds;
-    std::unordered_map<TerrainType, std::pair<int, int>> seedCoordsMap;
-    std::queue<Tile*> qu;
-    // generate seed coordinates for each biome except water lol
-    for (int i = 0; i < numOfBiomes; i++) {
-        if (static_cast<TerrainType>(i) == TerrainType::Water) {
-            continue;
+
+    mapThread = std::thread([this, &rng]() {
+        std::lock_guard<std::mutex> lock(mapMootex);
+        constexpr int numOfBiomes = static_cast<int>(TerrainType::COUNT_TT);
+        std::pair<int, int> seedCoords[numOfBiomes];
+
+        // quadrant_ranges[4/* the quadrant */][4]
+        // inner array indexes 0 = ymin 1 = ymax 2 = xmin 3 = xmax
+        int quadrant_ranges[4][4] = { {1, (Const::MAP_HEIGHT * 0.25f), 1, (Const::MAP_WIDTH * 0.25f)},
+                                         {(Const::MAP_HEIGHT * 0.25f) + 1, (Const::MAP_HEIGHT * 0.50f), (Const::MAP_WIDTH * 0.25f) + 1, (Const::MAP_WIDTH * 0.50f)},
+                                         {(Const::MAP_HEIGHT * 0.50f) + 1, (Const::MAP_HEIGHT * 0.75f), (Const::MAP_WIDTH * 0.50f) + 1, (Const::MAP_WIDTH * 0.75f)},
+                                         {(Const::MAP_HEIGHT * 0.75f) + 1, Const::MAP_HEIGHT, (Const::MAP_WIDTH * 0.75f) + 1, Const::MAP_WIDTH}
+        };
+
+        std::queue<Tile*> qu;
+        // generate seed coordinates for each biome except water lol
+        for (int i = 0; i < numOfBiomes; i++) {     // iterate through TerrainType
+            if (static_cast<TerrainType>(i) == TerrainType::Water) {
+                continue;
+            }
+
+            // randomly generate seed coords
+            int quadrant = i / 2;
+
+            int min_y = quadrant_ranges[quadrant][0];
+            int max_y = quadrant_ranges[quadrant][1];
+            int min_x = quadrant_ranges[quadrant][2];
+            int max_x = quadrant_ranges[quadrant][3];
+
+
+            seedCoords[i] = std::pair<int, int>(rng.generate_uniform_int(min_y, max_y - 1), rng.generate_uniform_int(min_x, max_x - 1));
+ 
+            // change tile biome type at seedCoords to current terrain type
+
+            auto& seed = this->tiles[seedCoords[i].first][seedCoords[i].second];
+            seed.biome = static_cast<TerrainType>(i);
+            // load tile into queue
+            qu.push(&seed);
         }
-        seedCoords[i] = std::pair<int, int>(rng.generate_uniform_int(0, Const::MAP_HEIGHT - 1), rng.generate_uniform_int(0, Const::MAP_WIDTH - 1));
-        seedCoordsMap.insert(std::pair<TerrainType, std::pair<int, int>>(static_cast<TerrainType>(i), seedCoords[i]));
-        tiles[seedCoords[i].first][seedCoords[i].second].biome = static_cast<TerrainType>(i);
-        seeds.push_back(&tiles[seedCoords[i].first][seedCoords[i].second]);
-    }
-    
-    // load seeds into queue
-    for (auto& seed: seeds) {
-        qu.push(seed);
-    }
-    float baseChance = 0.75f;
-    float falloff = 0.02f;
-    // loop the queue
-    while (!qu.empty()) {
-        Tile* tile = qu.front();
-        getNeighbors(*tile);
-        for (auto& neighbor : tile->neighbors) {
+   
 
+        // base chance for lat chance calculation
+        constexpr float baseChance = 0.75f;
+        constexpr float falloff = 0.01f;
+        // loop the queue
+        while (!qu.empty()) {
+            // grab the tile at the front of the line
+
+            Tile* tile = qu.front();
+
+            getNeighbors(*tile); // grab tile neighbors into cache
             
+            int biome_idx = static_cast<int>(tile->biome);
+            int seed_x = seedCoords[biome_idx].second;
+            int seed_y = seedCoords[biome_idx].first;
 
-            int dx = abs(static_cast<int>(neighbor->x - seedCoordsMap[tile->biome].first));
-            int dy = abs(static_cast<int>(neighbor->y - seedCoordsMap[tile->biome].second));
 
-            int manhattanDist = dx + dy;
-            float chance = 1.0f;
-            if (manhattanDist > 2) {
-                 chance = std::max(baseChance - (manhattanDist * falloff), 0.25f);
+            for (auto* neighbor : tile->neighbors) {
+
+                if (neighbor->beenSet)
+                    continue;
+                if (neighbor->biome != TerrainType::Water && neighbor->biome == tile->biome)
+                    continue;
+
+                // calculate coordinates of the difference of neighbor.x - seed.x, and neighbor.y - seed.y
+                int dx = abs(static_cast<int>(neighbor->x - seed_x));
+                int dy = abs(static_cast<int>(neighbor->y - seed_y));
+
+                int manhattanDist = dx + dy; // manhattan distance for simplicity
+                // if the manhattanDist is greater than 2 
+                // chance equals baseChance - (manhattanDist * falloff) clamped with a min of 23% chance
+                float chance = (manhattanDist > 10) 
+                    ? std::max(baseChance - (manhattanDist * falloff), 0.23f) 
+                    : 1.0f; // by default chance is set to 100
+                
+                if (rng.generate_uniform_float() < chance /* if random roll is less than chance */) {
+                    // set neighbors biome to tiles biome
+                    neighbor->biome = tile->biome;
+                    neighbor->beenSet = true;
+                    qu.push(neighbor); // push neighbor into queue
+                }
             }
-
-            float randomRoll = rng.generate_uniform_float();
-            // 60% chance of spreading biome to neighbor
-            if ((neighbor->biome == TerrainType::Water || neighbor->biome != tile->biome) && randomRoll < chance && !neighbor->beenSet)  {
-                neighbor->biome = tile->biome;
-                neighbor->beenSet = true;
-                qu.push(neighbor);
-            }
+            // pop value off queue. Don't forget to do this ;-;
+            qu.pop();
         }
-        qu.pop();
-    }
+    });
 }
 
 /// <summary>
 /// write a visual representation of the map to std::cout
 /// </summary>
 void Map::writeMapToCout() {
-    std::cout << '\n';
+    // buffer entire output to avoid per-symbol cout overhead
+    std::string buf;    
+    size_t row_count = tiles.size();
+    size_t col_count = row_count > 0 ? tiles[0].size() : 0;
+    buf.reserve(row_count * (col_count + 1) + 1);
+
+    buf += '\n';
     for (int i = 0; i < tiles.size(); i++) {
         for (auto& tile : tiles[i]) {
-            std::cout << Const::TerrainTypeSymbols[static_cast<size_t>(tile.biome)];
+            buf += Const::TerrainTypeSymbols[static_cast<size_t>(tile.biome)];
         }
-        std::cout << '\n';
+        buf += '\n';
     }
+    std::cout.write(buf.data(), static_cast<std::streamsize>(buf.size()));
 }
 /// <summary>
 /// write a visual representation of the map to a text file
